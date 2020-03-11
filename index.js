b0ceb5d1e8d6523635ac3b998a2fda6ccdbb74e9
mahiyana <r2d2ka@gmail.com>
2020-03-11 12:20:13 +0100

    unified indents

--- index.js
+++ index.js
@@ -2,49 +2,49 @@ var points = 0
 var question;
 
 document.getElementById("userAnswer").addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    checkAnswer();
-  }
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        checkAnswer();
+    }
 }); 
 
 function getQuestion(){
-  return Math.floor(Math.random() * 100); 
+    return Math.floor(Math.random() * 100); 
 }
 
 function getAnswer(question){
-  var answer;
-  if (question <= 9) {
-      answer = numbers["ones"][question];
-  } else if (question <= 19) {
-      answer = numbers["teens"][question];
-  } else if (question <= 99) {
-      answer = numbers["ties"][Math.floor(question/10)*10];
-      if (question % 10 !== 0) {
-          answer += " " + numbers["ones"][question - Math.floor(question/10)*10]
-      }
-  }
-  return answer;
+    var answer;
+    if (question <= 9) {
+        answer = numbers["ones"][question];
+    } else if (question <= 19) {
+        answer = numbers["teens"][question];
+    } else if (question <= 99) {
+        answer = numbers["ties"][Math.floor(question/10)*10];
+        if (question % 10 !== 0) {
+            answer += " " + numbers["ones"][question - Math.floor(question/10)*10]
+        }
+    }
+    return answer;
 }
 
 function checkAnswer(){
-  userAnswer = document.getElementById("userAnswer").value.trim();
-  properAnswer = getAnswer(parseInt(question));
-  if (userAnswer == properAnswer) {
-    var points = parseInt(document.getElementById("points").innerHTML);
-    points += 1;
-    document.getElementById("points").innerHTML = points;
-  } 
-  else {
-    alert("Prawidłowa odpowiedź to " + properAnswer + "!");
-  }
-  reloadQuestion();
+    userAnswer = document.getElementById("userAnswer").value.trim();
+    properAnswer = getAnswer(parseInt(question));
+    if (userAnswer == properAnswer) {
+        var points = parseInt(document.getElementById("points").innerHTML);
+        points += 1;
+        document.getElementById("points").innerHTML = points;
+    } 
+    else {
+        alert("Prawidłowa odpowiedź to " + properAnswer + "!");
+    }
+    reloadQuestion();
 }
 
 function reloadQuestion(){
-  question = getQuestion();
-  document.getElementById("question").innerHTML = question;
-  document.getElementById("userAnswer").value = "";
+    question = getQuestion();
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("userAnswer").value = "";
 }
 
 reloadQuestion();
--- numbers.js
+++ numbers.js
@@ -1,37 +1,36 @@
 numbers = {
-  "ones": {
-    "0": "nula",
-    "1": "jedan",
-    "2": "dva",
-    "3": "tri",
-    "4": "četiri",
-    "5": "pet",
-    "6": "šest",
-    "7": "sedam",
-    "8": "osam",
-    "9": "devet"
-  },
-  "teens": {
-    "10": "deset",
-    "11": "jedanaest",
-    "12": "dvanaest",
-    "13": "trinaest",
-    "14": "četrnaest",
-    "15": "petnaest",
-    "16": "šesnaest",
-    "17": "sedamnaest",
-    "18": "osamnaest",
-    "19": "devetnaest"
-  },
-  "ties": {
-    "20": "dvadeset",
-    "30": "trideset",
-    "40": "četrdeset",
-    "50": "pedeset",
-    "60": "šezdeset",
-    "70": "sedamdeset",
-    "80": "osamdeset",
-    "90": "devedeset",
-  }
-
+    "ones": {
+        "0": "nula",
+        "1": "jedan",
+        "2": "dva",
+        "3": "tri",
+        "4": "četiri",
+        "5": "pet",
+        "6": "šest",
+        "7": "sedam",
+        "8": "osam",
+        "9": "devet"
+    },
+    "teens": {
+        "10": "deset",
+        "11": "jedanaest",
+        "12": "dvanaest",
+        "13": "trinaest",
+        "14": "četrnaest",
+        "15": "petnaest",
+        "16": "šesnaest",
+        "17": "sedamnaest",
+        "18": "osamnaest",
+        "19": "devetnaest"
+    },
+    "ties": {
+        "20": "dvadeset",
+        "30": "trideset",
+        "40": "četrdeset",
+        "50": "pedeset",
+        "60": "šezdeset",
+        "70": "sedamdeset",
+        "80": "osamdeset",
+        "90": "devedeset",
+    }
 }
