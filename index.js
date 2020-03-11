e452eb0a9d3d94db1a633c7f6bcbec63771f1bbb
mahiyana <r2d2ka@gmail.com>
2020-03-11 11:33:03 +0100

    added questions for numbers 0-9

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-var question = "0"
-var answer = "nula";
 var points = 0
-document.getElementById("question").innerHTML = question;
+var question;
+
+function getQuestion(){
+  return Math.floor(Math.random() * 10); 
+}
+
+function getAnswer(question){
+  var answer;
+  if (question <= 9) {
+      answer = numbers["ones"][question];
+  }
+  return answer;
+}
 
 function checkAnswer(){
-  userAnswer = document.getElementById("userAnswer").value;
-  if (userAnswer.trim() == answer) {
+  userAnswer = document.getElementById("userAnswer").value.trim();
+  properAnswer = getAnswer(parseInt(question));
+  if (userAnswer == properAnswer) {
     var points = parseInt(document.getElementById("points").innerHTML);
     points += 1;
     document.getElementById("points").innerHTML = points;
   } 
   else {
-    alert("Prawidłowa odpowiedź to " + answer + "!");
+    alert("Prawidłowa odpowiedź to " + properAnswer + "!");
   }
+  reloadQuestion();
+}
+
+function reloadQuestion(){
+  question = getQuestion();
+  document.getElementById("question").innerHTML = question;
   document.getElementById("userAnswer").value = "";
 }
+
+reloadQuestion();
---
+++ numbers.js
@@ -0,0 +1,14 @@
+numbers = {
+  "ones": {
+    "0": "nula",
+    "1": "jedan",
+    "2": "dva",
+    "3": "tri",
+    "4": "četiri",
+    "5": "pet",
+    "6": "šest",
+    "7": "sedam",
+    "8": "osam",
+    "9": "devet"
+  }
+}
