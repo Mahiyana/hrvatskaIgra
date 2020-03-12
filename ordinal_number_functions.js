712604b289602bacdc2456b1a481541cb5b9fb83
mahiyana <r2d2ka@gmail.com>
2020-03-12 22:29:11 +0100

    made ordinal numbers questions work

--- ordinal_number_functions.js
+++ ordinal_number_functions.js
@@ -1,3 +1,64 @@
-function getOrdinalNumberQuestionAndAnswer(){
-    return ["3. kat", "treći kat"];
+function getOrdinalNumberQuestionAndAnswer(){ //TODO divide into two functions to make it testable (and more usable)
+    var question;
+    var answer;
+
+    // 0-masculine, 1-feminine, 2-neuter
+    var gender = Math.floor(Math.random() * 3); 
+    // 0-(1-99), 1-hundreds, 2-thousands
+   
+    var range = 0;
+    if (Math.floor(Math.random() * 10) === 0){ //chance 1 to 10
+        var range = Math.floor(Math.random() * 2) + 1;
+    }
+    
+    switch(range){
+        case 0:
+            question = Math.floor(Math.random() * 99) + 1;
+            if (question <= 9) {
+                answer = ordinalNumbers["ones"][question];
+            } else if (question <= 19) {
+                answer = cardinalNumbers["teens"][question];
+            } else if (question <= 99) {
+                answer = cardinalNumbers["ties"][Math.floor(question/10)*10];
+                if (question % 10 !== 0) {
+                    var ones = question % 10;
+                    answer += " " + ordinalNumbers["ones"][ones];
+                }
+            } 
+            break;
+        case 1:
+            question = (Math.floor(Math.random() * 9) + 1) * 100;
+            answer = ordinalNumbers["hundreds"][question];
+            break;
+        case 2:
+            question = (Math.floor(Math.random() * 10) + 1) * 1000;
+            answer = ordinalNumbers["thousands"][question];
+    }
+
+    var ordinableNoun;
+    switch(gender){
+        case 0:
+            answer += "i ";
+            ordinableNoun = ordinableNouns["masculine"][Math.floor(Math.random() * ordinableNouns["masculine"].length)];
+            break;
+        case 1:
+            answer += "a ";
+            ordinableNoun = ordinableNouns["feminine"][Math.floor(Math.random() * ordinableNouns["feminine"].length)];
+            break;
+        case 2:
+            if (question % 10 === 3) {
+                answer += "e ";
+            } else {
+                answer += "o ";
+            }    
+            ordinableNoun = ordinableNouns["neuter"][Math.floor(Math.random() * ordinableNouns["neuter"].length)];
+    }
+    question += ". " + ordinableNoun;
+    answer += ordinableNoun;
+
+
+    //return ["3. kat", "treći kat"];
+    return [question, answer];
 }
+
+
---
+++ ordinal_numbers_data.js
@@ -0,0 +1,43 @@
+ordinableNouns = {
+    "masculine": ["avion", "auto", "telefon", "poklon", "sladoled", "kofer", "autobus", "tata", "dio", "odgovor"],
+    "feminine": ["obitelj", "bilježnica", "olovka", "torba", "kuća", "rajčica", "rakija", "ploča", "razlika", "slika"],
+    "neuter": ["stablo", "more", "koljeno", "odijelo", "brdo", "grožđe", "pivo", "djelo", "društvo", "jaje"]
+}
+
+// obviously without last morpheme
+ordinalNumbers = {
+    "ones": {
+        "1": "prv",
+        "2": "drug",
+        "3": "treć",
+        "4": "četvrt",
+        "5": "pet",
+        "6": "šest",
+        "7": "sedam",
+        "8": "osam",
+        "9": "devet",
+    },
+    "hundreds": {
+       "100": "stot",
+       "200": "dvjestot",
+       "300": "tristot",
+       "400": "četiristot",
+       "500": "petstot",
+       "600": "šeststot",
+       "700": "sedamstot",
+       "800": "osamstot",
+       "900": "devetstoti",
+    },
+    "thousands": {
+        "1000": "tisićit",
+        "2000": "dvije tisićit",
+        "3000": "tri tisićit",
+        "4000": "četiri tisićit",
+        "5000": "pet tisićit",
+        "6000": "šest tisićit",
+        "7000": "sedam tisićit",
+        "8000": "osam tisićit",
+        "9000": "devet tisićit",
+        "10000": "deset tisićit",
+    }
+}
