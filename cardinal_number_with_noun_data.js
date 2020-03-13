7989ddd8ea82da789b86d5fe57c43b3d00308562
mahiyana <r2d2ka@gmail.com>
2020-03-13 01:14:24 +0100

    made cardinal numbers with nouns questions work

---
+++ cardinal_number_with_noun_data.js
@@ -0,0 +1,25 @@
+// [Nominative singular, Genitive singular, Genitive plural]
+nounsWithCases = [
+    ["stol", "stola", "stolova"],
+    ["kralj", "kralja", "kraljeva"],
+    ["muškarac", "muškarca", "muškaraca"],
+    ["park", "parka", "parkova"],
+    ["student", "studenta", "studenata"],
+    ["čovjek", "čovjeka", "ljudi"],
+    ["majmum", "majmuna", "majmuna"],
+    ["metar", "metra", "metara"], 
+
+    ["zemlja", "zemlje", "zemjla"],
+    ["pjesma", "pjesme", "pjesama"], //?
+    ["olovka", "olovke", "olovki"],
+    ["djevojka", "djevojke", "djevojaka"],
+    ["karta", "karte", "karata"],
+    ["litra", "litre", "litara"],
+    ["žena", "žene", "žena"],
+    ["lopta", "lopte", "lopti"],
+
+    ["pismo", "pisma", "pisama"],
+    ["koljeno", "koljena", "koljena"],
+    ["selo", "sela", "sela"],
+    ["more", "mora", "mora"],
+]
--- cardinal_number_with_noun_functions.js
+++ cardinal_number_with_noun_functions.js
@@ -1,3 +1,31 @@
-function getCardinalNumberWithNounQuestionAndAnswer(){
-    return ["stol ☓3", "tri stola"];
+function getCardinalNumberWithNounQuestionAndAnswer(){ //TODO divide into two functions to make it testable (and more usable)
+    var nounSet = nounsWithCases[Math.floor(Math.random() * nounsWithCases.length)];
+    var questionNumber = Math.floor(Math.random() * 99) + 1;
+    var question = nounSet[0] + " ☓" + questionNumber;
+    var answer;
+    var isATeen = false;
+
+    if (questionNumber <= 9) {
+        answer = cardinalNumbers["ones"][questionNumber];
+    } else if (questionNumber <= 19) {
+        answer = cardinalNumbers["teens"][questionNumber];
+        isATeen = true;
+    } else {
+        answer = cardinalNumbers["ties"][Math.floor(questionNumber/10)*10];
+        if (questionNumber % 10 !== 0) {
+            var ones = questionNumber % 10;
+            answer += " " + cardinalNumbers["ones"][ones];
+        }
+    }
+    
+    lastDigit = questionNumber % 10;
+    if (lastDigit === 2 || lastDigit === 3 || lastDigit === 4) {
+        answer += " " + nounSet[1];
+    } else if (!isATeen && lastDigit == 1) {
+       answer += " " + nounSet[0];
+    } else {
+       answer += " " + nounSet[2];
+    }
+
+    return [question, answer];
 }
